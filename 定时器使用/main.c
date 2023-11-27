1ab48698f558706520869f9ce4023a4a21f5438a
HysterLc <M1175054489@outlook.com>
2023-11-27 20:52:31 +0800

    Third

--- 定时器使用/main.c
+++ 定时器使用/main.c
@@ -3,11 +3,11 @@
 #include "Timer0.h"
 
 unsigned char KeyNum;
+unsigned char  LEDMode =0;
 void main()
 {
    Timer0_Init();
    P2=0xFE;
-		int LEDMode =0;
     while(1)
     {
 		
@@ -24,18 +24,19 @@ void main()
 void  Timer0_Routine() interrupt 1
 {
 	static unsigned int T0Count;
-	unsigned 
    TH0=64609/256;
    TL0=64609%256;
    T0Count++;
-   if(T0Count>=100)
+   if(T0Count>=500)
    {
        T0Count=0;
        P2_1=~P2_1;
 	   if(LEDMode==0)
 	   		P2=_crol_(P2,1);
+        
 		if(LEDMode==1)
-			P2=_crol_(P2,1);
+			P2=_cror_(P2,1);
+     
    }
 }
 
